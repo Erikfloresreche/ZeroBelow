8bf83610f4fb2ec0898bfb94d354f2ffdb60b086
Erik Flores Reche <erikfloresreche@gmail.com>
2025-09-14 11:00:58 +0200

    Add Services component and card widget

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import Forest from "./assets/images/Forest.jpg"
 import { Header } from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
+import { Services } from "./components/Services/Services";
 
 
 export default function App() {
@@ -24,6 +25,7 @@ export default function App() {
       <div className="relative z-20">
         <Header />
         <Hero />
+        <Services />
       </div>
     </div>
   );
